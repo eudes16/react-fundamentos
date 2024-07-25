85982da216ea6756606adfeb98b4e2990ea9a0fc
Eudes Silva <eudes.vss@gmail.com>
2024-07-25 19:15:33 -0300

    Teste deploy

--- dist/server/server-reference-manifest.js
+++ dist/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"5/bPfK00LwaGflWPlEpt9ged9vJfazt2ANoW3QuE00g=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"OMzvIjG6eNzKnsTHdG/owI/x6O6W4b/oWroixtkZ90E=\"}"
